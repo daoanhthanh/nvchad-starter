50b4dae88fb1dd44e2cfa3a2a32b82c69e256808
Đào Anh Thành <daoanhthanh.work@gmail.com>
2025-02-17 21:58:09 +0700

    update 2 plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,14 +12,31 @@ return {
       require "configs.lspconfig"
     end,
   },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+        "html", "css", "php", "json",
+        "yaml", "toml", "javascript", "typescript",
+        "bash", "python", "c", "cpp", "rust",
+        "java", "go", "lua", "ruby", "svelte",
+  		},
+  	},
+  },
+  -- lightspeed.nvim
+  {
+    'ggandor/lightspeed.nvim',
+    event = 'VimEnter',
+  },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  -- nvim-surround
+  {
+    "kylechui/nvim-surround",
+    -- version = "å*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({})
+    end
+  },
 }
